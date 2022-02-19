599f3ecaf432124d3965e3a97556edbeb3c2c56e
carloradice <c.radice@campus.unimib.it>
2022-02-19 14:33:19 +0100

    modified kitti

--- gen_data_kitti.py
+++ gen_data_kitti.py
@@ -33,8 +33,8 @@ SEQ_LENGTH = 3
 WIDTH = 416
 HEIGHT = 128
 STEPSIZE = 1
-INPUT_DIR = '/usr/local/google/home/anelia/struct2depth/KITTI_FULL/kitti-raw-uncompressed'
-OUTPUT_DIR = '/usr/local/google/home/anelia/struct2depth/KITTI_procesed/'
+INPUT_DIR = '/media/RAIDONE/radice/datasets/kitti/data'
+OUTPUT_DIR = '/media/RAIDONE/radice/datasets/kitti/struct2depth'
 
 
 def get_line(file, start):
@@ -53,44 +53,20 @@ def get_line(file, start):
     return ret
 
 
-def crop(img, segimg, fx, fy, cx, cy):
-    # Perform center cropping, preserving 50% vertically.
-    middle_perc = 0.50
-    left = 1-middle_perc
-    half = left/2
-    a = img[int(img.shape[0]*(half)):int(img.shape[0]*(1-half)), :]
-    aseg = segimg[int(segimg.shape[0]*(half)):int(segimg.shape[0]*(1-half)), :]
-    cy /= (1/middle_perc)
-
-    # Resize to match target height while preserving aspect ratio.
-    wdt = int((128*a.shape[1]/a.shape[0]))
-    x_scaling = float(wdt)/a.shape[1]
-    y_scaling = 128.0/a.shape[0]
-    b = cv2.resize(a, (wdt, 128))
-    bseg = cv2.resize(aseg, (wdt, 128))
-
-    # Adjust intrinsics.
-    fx*=x_scaling
-    fy*=y_scaling
-    cx*=x_scaling
-    cy*=y_scaling
-
-    # Perform center cropping horizontally.
-    remain = b.shape[1] - 416
-    cx /= (b.shape[1]/416)
-    c = b[:, int(remain/2):b.shape[1]-int(remain/2)]
-    cseg = bseg[:, int(remain/2):b.shape[1]-int(remain/2)]
-
-    return c, cseg, fx, fy, cx, cy
-
-
 def run_all():
   ct = 0
+
+black = np.zeros((HEIGHT, WIDTH*3))
+
 if not OUTPUT_DIR.endswith('/'):
     OUTPUT_DIR = OUTPUT_DIR + '/'
 
 for d in glob.glob(INPUT_DIR + '/*/'):
     date = d.split('/')[-2]
+
+    if date == 'test-velodyne-gt':
+        continue
+
     file_calibration = d + 'calib_cam_to_cam.txt'
     calib_raw = [get_line(file_calibration, 'P_rect_02'), get_line(file_calibration, 'P_rect_03')]
 
@@ -99,9 +75,9 @@ for d in glob.glob(INPUT_DIR + '/*/'):
         print('Processing sequence', seqname)
         for subfolder in ['image_02/data', 'image_03/data']:
             ct = 1
-            seqname = d2.split('/')[-2] + subfolder.replace('image', '').replace('/data', '')
-            if not os.path.exists(OUTPUT_DIR + seqname):
-                os.mkdir(OUTPUT_DIR + seqname)
+            seqname = d2.split('/')[-2]
+            if not os.path.exists(os.path.join(OUTPUT_DIR, date, seqname, subfolder)):
+                os.makedirs(os.path.join(OUTPUT_DIR, date, seqname, subfolder))
 
             calib_camera = calib_raw[0] if subfolder=='image_02/data' else calib_raw[1]
             folder = d2 + subfolder
@@ -135,11 +111,14 @@ for d in glob.glob(INPUT_DIR + '/*/'):
                     img = cv2.resize(img, (WIDTH, HEIGHT))
                     big_img[:,wct*WIDTH:(wct+1)*WIDTH] = img
                     wct+=1
-                cv2.imwrite(OUTPUT_DIR + seqname + '/' + imgnum + '.png', big_img)
-                f = open(OUTPUT_DIR + seqname + '/' + imgnum + '_cam.txt', 'w')
+
+                cv2.imwrite(os.path.join(OUTPUT_DIR, date, seqname, subfolder, '{}.png'.format(imgnum)), big_img)
+                cv2.imwrite(os.path.join(OUTPUT_DIR, date, seqname, subfolder, '{}-fseg.png'.format(imgnum)), black)
+                f = open(os.path.join(OUTPUT_DIR, date, seqname, subfolder, '{}_cam.txt'.format(imgnum)), 'w')
                 f.write(calib_representation)
                 f.close()
                 ct+=1
+
 
 def main(_):
   run_all()
